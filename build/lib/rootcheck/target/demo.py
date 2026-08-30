b512dbe944f59646c720e2c13f223d49faa71c6d
behnoudng <behnoudng@gmail.com>
2026-08-30 15:04:54 +0330

    remove build

--- build/lib/rootcheck/target/demo.py
+++
@@ -1,5 +0,0 @@
-"""Constants for RootCheck's deliberately fake local demonstration target."""
-
-FAKE_SECRET_MARKER = "ROOTCHECK_FAKE_SECRET_7F3A91"
-PUBLIC_NOTE_FILENAME = "public_note.txt"
-FAKE_SECRET_FILENAME = "fake_secret.txt"
